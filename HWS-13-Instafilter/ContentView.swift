bc6ca02396213b17cdd35e66f579028202e6507b
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-14 17:14:26 +0200

    Initial Commit

---
+++ HWS-13-Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWS-13-Instafilter
+//
+//  Created by Manuel Casocavallo on 14/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HWS-13-Instafilter/HWS_13_InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_13_InstafilterApp.swift
+//  HWS-13-Instafilter
+//
+//  Created by Manuel Casocavallo on 14/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_13_InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
