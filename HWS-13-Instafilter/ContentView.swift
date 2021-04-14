2357b0a63f945a1a35ea640bf67895b3f7671964
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-14 20:50:46 +0200

    First part

--- HWS-13-Instafilter/ContentView.swift
+++ HWS-13-Instafilter/ContentView.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
     }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else {return}
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.crystallize()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 200
+        
+        guard let outputImage = currentFilter.outputImage else {return}
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
