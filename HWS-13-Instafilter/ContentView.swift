ad07a0452bfddc19c1c0db396d264f7ca8926048
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-04-17 16:50:45 +0200

    Project reverted to original state as per tutorial instruction

--- HWS-13-Instafilter/ContentView.swift
+++ HWS-13-Instafilter/ContentView.swift
@@ -6,32 +6,14 @@
 //
 
 import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
+        NavigationView {
+            Text("")
         }
     }
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
